bc3a551c17373d906dee70d3ea8390b905b75512
Clemens Tiedt <clemens@tiedt.dev>
2025-01-22 09:54:18 +0100

    update to newer librasta

--- build.rs
+++ build.rs
@@ -2,22 +2,18 @@ use std::{env, path::PathBuf};
 
 fn main() {
     // these headers cause problems with circular includes, so we don't generate bindings for them
-    let disallowed_headers = vec!["fifo.h".into(), "rastaredundancy.h".into()];
+    let disallowed_headers = vec!["rastarole.h".into()];
 
     println!("cargo:rerun-if-changed=rasta-sys");
 
-    // Right now, we have to overwrite the cmake options because librasta will not compile with -Werror.
-    std::fs::copy(concat!(env!("CARGO_MANIFEST_DIR"), "/CompileOptions.cmake"), "rasta-protocol/cmake/CompileOptions.cmake").expect("Failed to copy CmakeOptions file");
-
-    let mut dst = cmake::build("rasta-protocol");
+    let mut dst = cmake::build("librasta");
     dst.push("lib");
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=dylib=rasta");
 
-    let mut bindings = bindgen::Builder::default().clang_arg("-Irasta-protocol/src/include/");
-    for header in
-        std::fs::read_dir("rasta-protocol/src/include/rasta").expect("Failed to read directory")
+    let mut bindings = bindgen::Builder::default().clang_arg("-Ilibrasta/src/include/");
+    for header in std::fs::read_dir("librasta/src/include/rasta").expect("Failed to read directory")
     {
         let header = header.unwrap();
         if !disallowed_headers.contains(&header.file_name().to_string_lossy().into_owned()) {
